a03056e58911e979914e26560c4f2d1da02dd365
vladyslavkosenko <vaduha91@gmail.com>
2022-10-11 11:01:38 +0300

    Implemented custom login and logout handlers

---
+++ src/main/java/com/example/springsecurity/controller/SuccessController.java
@@ -0,0 +1,14 @@
+package com.example.springsecurity.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SuccessController {
+
+    @GetMapping("/success")
+    public String getSuccessPage() {
+        return "success";
+    }
+}
